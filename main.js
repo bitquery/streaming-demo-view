32b77593d920f9d37be57d9d682ab9d92a1d81f0
Spendil <spendil@mail.ru>
2023-01-13 14:56:20 +0300

    streaming-demo-view

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ setInterval(() => document.getElementById("datetime").innerHTML = new Date().toL
 }), 1)
 
 const client = createClient({
-    url: 'wss://streaming.bitquery.io/graphql'
+    url: 'ws://streaming.bitquery.io/graphql'
 });
 
 const maxSize = 10
